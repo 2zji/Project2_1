321cebe8979111cd32194ddc43f7758a8c1af00e
Mirim <Mirim@DESKTOP-JHIHQUE>
2025-03-12 09:15:16 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ㅣ
+        }
+    }
+}
