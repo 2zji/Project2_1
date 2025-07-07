2f929b55f13dc0947f683b1aa5d7357f1c7cfbe0
Mirim <s2313@e-mirm.hs.kr>
2025-07-07 14:01:52 +0900

    test

--- Program.cs
+++ Program.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project2_1
+namespace Project02_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ㅣ
+            int a, b;
+            // 나중에 입력 받을 예정
+
+            int result;
+
+            a = 100;
+            b = 50;
+
+            result = a + b;
+            Console.WriteLine(a + "+" + b + "=" + result); // operator 
+
+            result = a - b;
+            Console.WriteLine(a + "-" + b + "=" + result); // operator 
+
+            result = a * b;
+            Console.WriteLine(a + "*" + b + "=" + result); // operator 
+
+            result = a / b;
+            Console.WriteLine(a + "/" + b + "=" + result); // operator 
+
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Project2_1")]
+[assembly: AssemblyTitle("Project02_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Project2_1")]
+[assembly: AssemblyProduct("Project02_1")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("9bee1f83-1a2e-4af5-bf5f-4cc72b8082e2")]
+[assembly: Guid("d56815db-4c25-4ecc-b8a3-6653a6640a10")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
